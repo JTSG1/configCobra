6ba4539f42390720b212ba9dcb7c32523cf43a40
JTSG1 <james@tesh.digital>
2024-05-15 15:48:22 +0100

    feat: updated envs

--- src/settings/settings.py
+++ src/settings/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 # Read .env file
 
 DB_NAME = os.getenv('CONFIG_COBRA_DB_NAME')
-DB_HOST = os.getenv('CONFIG_COBRA_DB_USER')
+DB_HOST = os.getenv('CONFIG_COBRA_DB_HOST')
 DB_PORT = os.getenv('CONFIG_COBRA_DB_PORT')
-DB_USER = os.getenv('CONFIG_COBRA_DB_HOST')
+DB_USER = os.getenv('CONFIG_COBRA_DB_USER')
 DB_PASSWORD = os.getenv('CONFIG_COBRA_DB_PASSWORD')
