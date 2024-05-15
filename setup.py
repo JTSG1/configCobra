1d9c3ccfa645128c24086df2d105524ceca56f2c
JTSG1 <james@tesh.digital>
2024-05-15 15:46:30 +0100

    feat: updated envs

--- setup.py
+++ setup.py
@@ -4,9 +4,17 @@ from setuptools import setup, find_packages
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name='config-cobra',
-    version='0.1.0',
+    version='0.1.3',
+    description='A simple yaml to database updater',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/JTSG1/configCobra',
+    license='MIT',
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
--- src/settings/settings.py
+++ src/settings/settings.py
@@ -6,6 +6,6 @@ load_dotenv()
 
 DB_NAME = os.getenv('CONFIG_COBRA_DB_NAME')
 DB_HOST = os.getenv('CONFIG_COBRA_DB_USER')
-DB_PORT = os.getenv('CONFIG_COBRA_DB_PASSWORD')
+DB_PORT = os.getenv('CONFIG_COBRA_DB_PORT')
 DB_USER = os.getenv('CONFIG_COBRA_DB_HOST')
-DB_PASSWORD = os.getenv('CONFIG_COBRA_DB_PORT')
+DB_PASSWORD = os.getenv('CONFIG_COBRA_DB_PASSWORD')
